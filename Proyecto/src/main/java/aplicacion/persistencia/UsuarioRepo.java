b9e21c577fb04dca81959db9aa4ad448af742847
Cristian <robertcs62@educastur.es>
2022-12-13 13:59:05 +0100

    UsuarioRepo hecho

---
+++ Proyecto/src/main/java/aplicacion/persistencia/UsuarioRepo.java
@@ -0,0 +1,12 @@
+package aplicacion.persistencia;
+ 
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import aplicacion.modelo.Usuario;
+public interface UsuarioRepo extends JpaRepository<Usuario, Integer> {
+
+	public Optional<Usuario> findByDni(String dni);
+	
+}
