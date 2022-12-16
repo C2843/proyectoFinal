c683fc3dfdf0acf0e42bc7526fbb52a523813cc7
Cristian <robertcs62@educastur.es>
2022-12-16 12:44:16 +0100

    Eliminar animales en explotacion y solucionar error id usuario en casa

--- Proyecto/src/main/java/aplicacion/controlador/ExplotacionController.java
+++ Proyecto/src/main/java/aplicacion/controlador/ExplotacionController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import aplicacion.modelo.Animal;
 import aplicacion.modelo.Explotacion;
+import aplicacion.modelo.Usuario;
 import aplicacion.persistencia.AnimalDAO;
 import aplicacion.persistencia.AnimalRepo;
 import aplicacion.persistencia.ExplotacionDAO;
 import aplicacion.persistencia.ExplotacionRepo;
+import aplicacion.persistencia.UsuarioRepo;
 
 @RequestMapping("/explotaciones")
 @Controller
@@ -26,6 +28,8 @@ public class ExplotacionController {
 	private AnimalRepo animalRepo;
 	@Autowired
 	private ExplotacionRepo explotacionRepo;
+	@Autowired
+	private UsuarioRepo usuarioRepo;
 	AnimalDAO animalDAO = new AnimalDAO();
 	ExplotacionDAO explotacionDAO = new ExplotacionDAO();
 
@@ -34,7 +38,9 @@ public class ExplotacionController {
 
 		ArrayList<Explotacion> misExplotaciones = (ArrayList<Explotacion>) explotacionRepo.findAll();
 		ArrayList<Animal> misAnimales = (ArrayList<Animal>) animalRepo.findAll();
+		ArrayList<Usuario>misUsuarios = (ArrayList<Usuario>) usuarioRepo.findAll();
 		model.addAttribute("listaExplotaciones", misExplotaciones);
+		model.addAttribute("listaUsuarios", misUsuarios);
 		model.addAttribute("listaAnimales", misAnimales);
 		model.addAttribute("explotacionNueva", new Explotacion());
 		model.addAttribute("explotacionEditada", new Explotacion());
